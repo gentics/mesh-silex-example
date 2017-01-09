c4a7a3b7d5a689ca07fcd051718e38a1fbfe9efc
Johannes Schüth <j.schueth@gentics.com>
2017-01-09 16:51:28 +0100

    Update templates

--- src/index.php
+++ src/index.php
@@ -8,19 +8,52 @@ use Symfony\Component\HttpFoundation\Response;
 $app = new Silex\Application();
 $app['debug'] = true;
 
+define("BASEURI", "http://admin:admin@mesh:8080/api/v1/");
+
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
-	'twig.path' => __DIR__.'/views',
+    'twig.path' => __DIR__.'/views',
 ));
 
+function loadBreadcrumbData() {
+    $uri = BASEURI . "demo/navroot/?maxDepth=1&resolveLinks=short";
+    $response = \Httpful\Request::get($uri)->send();
+    //print_r($response->body->root->children);
+    return $response->body->root->children;
+}
+
+function loadChildren($uuid) {
+    $uri = BASEURI . "demo/nodes/". $uuid . "/children?expandAll=true&resolveLinks=short";
+    $response =  \Httpful\Request::get($uri)->send();
+    return $response->body->data;
+}
 
 $app->get('/{path}', function (Request $request, $path) use ($app) {
-  $uri = "http://admin:admin@mesh:8080/api/v1/demo/webroot/" . rawurlencode($path);
-  $response = \Httpful\Request::get($uri)->send();
-  //print_r($path);
-  //print_r($response->body->fields);
-  return $app['twig']->render('start.twig', array(
-    'fields'=> $response->body->fields)
-  );
+  if ($path == "/" || $path=="") {
+    return $app['twig']->render('welcome.twig', array('children' => getNavigationData()));
+  } else {
+    $uri = BASEURI . "demo/webroot/" . rawurlencode($path) . "?resolveLinks=short";
+    $response = \Httpful\Request::get($uri)->send();
+    if (substr($response->content_type, 0, 6) === "image/") {
+      return \Httpful\Request::get($uri)->parseWith(function($body) {
+        return explode(",", $body);
+      })->send();
+    } else {
+      $uuid = $response->body->uuid;
+      $children = loadChildren($uuid);
+      if ($response->body->schema->name === "vehicle") {
+        return $app['twig']->render('productDetail.twig', array(
+          'breadcrumb' => loadBreadcrumbData(),
+          'product' => $response->body)
+        );
+      } else {
+        return $app['twig']->render('productList.twig', array(
+          'breadcrumb' => loadBreadcrumbData(),
+          'category'=> $response->body,
+          'products' => $children)
+        );
+      }
+    }
+  }
 })->assert("path", ".*");
 
 $app->run();
