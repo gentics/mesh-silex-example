f242b9a51e03e23d12c7a83199e040a86058e043
Johannes Schüth <j.schueth@gentics.com>
2017-01-09 17:21:04 +0100

    Add documentation and fix duplicate image loading issue

--- src/index.php
+++ src/index.php
@@ -14,38 +14,51 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
 ));
 
-function loadBreadcrumbData() {
+/**
+ * Load the breadcrumb information for the root level of the project.
+ * @return Array with breadcrumb information
+ */
+function loadBreadcrumbData(): array {
     $uri = BASEURI . "demo/navroot/?maxDepth=1&resolveLinks=short";
     $response = \Httpful\Request::get($uri)->send();
-    //print_r($response->body->root->children);
     return $response->body->root->children;
 }
 
-function loadChildren($uuid) {
+/**
+ * Load a list of children for the specified node.
+ * @param uuid Uuid of the node
+ */
+function loadChildren(string $uuid): array {
     $uri = BASEURI . "demo/nodes/". $uuid . "/children?expandAll=true&resolveLinks=short";
     $response =  \Httpful\Request::get($uri)->send();
     return $response->body->data;
 }
 
-$app->get('/{path}', function (Request $request, $path) use ($app) {
-  if ($path == "/" || $path=="") {
+// Main route handler
+$app->get('/{path}', function (Request $request, string $path) use ($app) {
+
+  // Handle index/welcome page
+  if ($path === "/" || $path === "") {
     return $app['twig']->render('welcome.twig', array('breadcrumb' => loadBreadcrumbData()));
   } else {
     $uri = BASEURI . "demo/webroot/" . rawurlencode($path) . "?resolveLinks=short";
     $response = \Httpful\Request::get($uri)->send();
+
+    // Check whether the found node represents an image. Otherwise continue with template specific code.
     if (substr($response->content_type, 0, 6) === "image/") {
-      return \Httpful\Request::get($uri)->parseWith(function($body) {
-        return explode(",", $body);
-      })->send();
+      return $response->raw_body;
     } else {
       $uuid = $response->body->uuid;
       $children = loadChildren($uuid);
+
+      // Check whether the loaded node is an vehicle node. In those cases a detail page should be shown.
       if ($response->body->schema->name === "vehicle") {
         return $app['twig']->render('productDetail.twig', array(
           'breadcrumb' => loadBreadcrumbData(),
           'product' => $response->body)
         );
       } else {
+        // In all other cases the node can only be a category. Display the product list for those cases.
         return $app['twig']->render('productList.twig', array(
           'breadcrumb' => loadBreadcrumbData(),
           'category'=> $response->body,
@@ -54,6 +67,8 @@ $app->get('/{path}', function (Request $request, $path) use ($app) {
       }
     }
   }
+
+// Prevent silex from handling slashes in the request path
 })->assert("path", ".*");
 
 $app->run();
